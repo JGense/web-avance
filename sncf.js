3babfec9369e90d3e975b67b65875825476628a3
Jimmy <jimmy.gense@lacatholille.fr>
2019-12-02 10:13:41 +0100

    Function for train stations searching

---
+++ sncf.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+async function searchSncf(query) {
+    let reponseApiSncf = await fetch(`https://ressources.data.sncf.com/api/records/1.0/search/?dataset=referentiel-gares-voyageurs&q=${query}`).catch((error) => console.log(error));
+    let jsonReponseSncf = await reponseApiSncf.json().catch((error) => {console.log(error)});
+    return jsonReponseSncf.records.map(function(gare) {
+        return gare.fields.gare_ut_libelle;
+    });
+}
+
+async function printResultats() {
+    const arrayGares = await searchSncf('besancon');
+    console.log(arrayGares);
+}
+
+printResultats();
+
